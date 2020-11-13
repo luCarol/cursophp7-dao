b16787076fe65d707bab32b8670c907ac3e3a348
luCarol <luanacaroliny07@gmail>
2020-11-13 09:39:52 -0300

    Métodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -1,5 +1,7 @@
 <?php
 
+use FFI\Exception;
+
 class Usuario {
     private $idusuario;
     private $deslogin;
@@ -66,6 +68,44 @@ class Usuario {
             "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
         ));
     }
+
+    public static function getList() {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+    }
+
+    public static function search($login) {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    public function login($login, $password) {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND desenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if (count($results) > 0) {
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDesenha($row['desenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        } else {
+            throw new Exception("Login e/ou senha inválidos");
+        }
+    }
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -8,10 +8,22 @@ require_once("config.php");
 
 // echo json_encode($usuarios);
 
-$root = new Usuario();
+// Carrega um único usuário
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-$root->loadById(3);
+// Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+// Carrega uma lista ed usuários buscando pelo login
+// $search = Usuario::search("jo");
+// echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+echo $usuario;
 
 ?>
