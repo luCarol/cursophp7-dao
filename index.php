75429b882c140b46464d6829735a9a73ce9342fb
luCarol <luanacaroliny07@gmail>
2020-11-13 10:24:03 -0300

    Comando delete

--- index.php
+++ index.php
@@ -33,9 +33,15 @@ require_once("config.php");
 // $aluno->insert();
 // echo $aluno;
 
+// Alterar um usuário
+// $usuario = new Usuario();
+// $usuario->loadById(9);
+// $usuario->update("professor", "!@#$%&*");
+// echo $usuario;
+
 $usuario = new Usuario();
-$usuario->loadById(9);
-$usuario->update("professor", "!@#$%&*");
+$usuario->loadById(8);
+$usuario->delete();
 echo $usuario;
 
 ?>
